38936f4571b4510282d8bb01a66629c07d667d4c
Helium Yang <47203690+HeliumYang@users.noreply.github.com>
2023-11-17 16:42:34 -0500

    -

--- scriptEdit.js
+++ scriptEdit.js
@@ -1,30 +1,37 @@
-var fileUpload = document.querySelector('#upload-script');
 let script;
 var savefile;
 
-fileUpload.addEventListener("change", () => {
-    let scriptFile = fileUpload.files;
-    if (scriptFile.length == 0) return;
+function openUpload() {
+    document.querySelector("#loadScript").innerHTML = '<a onclick="closeUpload()">Load Script</a><input type="file" id="upload-script">';
+    var fileUpload = document.querySelector('#upload-script');
+    fileUpload.addEventListener("change", () => {
+        let scriptFile = fileUpload.files;
+        if (scriptFile.length == 0) return;
     
-    const file = scriptFile[0];
+        const file = scriptFile[0];
  
-    let reader = new FileReader();
+        let reader = new FileReader();
  
-    reader.onload = (e) => {
-        const file = e.target.result;
+        reader.onload = (e) => {
+            const file = e.target.result;
  
-        // This is a regular expression to identify carriage 
-        // Returns and line breaks
-        const lines = file.split(/\r\n|\n/);
-        script = lines.join('\n');
+            // This is a regular expression to identify carriage 
+            // Returns and line breaks
+            const lines = file.split(/\r\n|\n/);
+            script = lines.join('\n');
+            closeUpload();
+        };
  
-    };
+        reader.onerror = (e) => alert(e.target.error.name);
  
-    reader.onerror = (e) => alert(e.target.error.name);
- 
-    reader.readAsText(file);
+        reader.readAsText(file);
 
-}); // fileUpload event listener
+    }); // fileUpload event listener
+} // openUpload
+
+function closeUpload() {
+    document.querySelector("#loadScript").innerHTML = '<a onclick="openUpload()">Load Script</a>'
+} // closeUpload
 
 function Script(docType, name) {
     this.name = name;
@@ -39,19 +46,26 @@ function Script(docType, name) {
 function Character(name) {
     this.name = name;
     this.alt = [];
+    this.starred = false;
+    this.notes = "";
 } // Character
 
 function Act() {
     this.label = "";
     this.scenes = [];
+    this.starred = false;
+    this.notes = "";
 } // Act
 
 function Scene() {
     this.label = "";
+    this.starred = false;
+    this.notes = "";
 } // Scene
 
 function Line() {
-
+    this.notes = "";
+    this.starred = false;
 } // Line
 
 function rmChar(name) {
@@ -59,6 +73,8 @@ function rmChar(name) {
     if (charIndex >= 0) {
         rmCharLines(charIndex);
         reorderCharIndex(charIndex);
+        rmCharAssociation(charIndex);
+        reorderCharAssociation(charIndex);
     } // if
 
 } // rmChar
